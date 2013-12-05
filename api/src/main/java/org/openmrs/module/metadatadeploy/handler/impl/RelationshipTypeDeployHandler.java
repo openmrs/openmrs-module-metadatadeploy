40279316c29162b93eb2aaefb06470c83f514447
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2013-12-05 14:28:58 +0300

    Added relationship type handle updated the constructor class to provide methods to hanldle the relationship types

---
+++ api/src/main/java/org/openmrs/module/metadatadeploy/handler/impl/RelationshipTypeDeployHandler.java
@@ -0,0 +1,11 @@
+package org.openmrs.module.metadatadeploy.handler.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: codehub
+ * Date: 12/5/13
+ * Time: 12:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RelationshipTypeDeployHandler {
+}
