6f05dac972c4a06512db460760981dd8001d92d1
Rowan Seymour <rowanseymour@gmail.com>
2014-01-23 11:22:10 +0200

    Removed unused import

--- api/src/main/java/org/openmrs/module/metadatadeploy/api/impl/MetadataDeployServiceImpl.java
+++ api/src/main/java/org/openmrs/module/metadatadeploy/api/impl/MetadataDeployServiceImpl.java
@@ -14,7 +14,6 @@
 
 package org.openmrs.module.metadatadeploy.api.impl;
 
-import antlr.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.OpenmrsObject;
