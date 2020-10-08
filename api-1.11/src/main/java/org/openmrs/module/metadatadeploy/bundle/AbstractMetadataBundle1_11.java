753d7459e7ef71dc34db70f769b94b92650798e4
Cosmin <cioan@pih.org>
2020-10-08 14:22:51 -0400

    DPLY-52, support installing PatientIdentifierType with UniquenessBehavior (#12)
    * DPLY-52, support installing PatientIdentifierType with UniquenessBehavior

---
+++ api-1.11/src/main/java/org/openmrs/module/metadatadeploy/bundle/AbstractMetadataBundle1_11.java
@@ -0,0 +1,12 @@
+package org.openmrs.module.metadatadeploy.bundle;
+
+import org.openmrs.annotation.OpenmrsProfile;
+import org.openmrs.module.metadatadeploy.descriptor.PatientIdentifierTypeDescriptor1_11;
+
+@OpenmrsProfile(openmrsVersion = "1.11.0")
+public abstract class AbstractMetadataBundle1_11 extends AbstractMetadataBundle {
+
+    protected void install(PatientIdentifierTypeDescriptor1_11 d) {
+        install(CoreConstructors1_11.patientIdentifierType(d.name(), d.description(), d.format(), d.formatDescription(), d.validator(), d.locationBehavior(), d.uniquenessBehavior(), d.required(), d.uuid()));
+    }
+}
